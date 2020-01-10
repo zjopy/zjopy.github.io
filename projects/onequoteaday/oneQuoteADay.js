d19dcf290a7e2c83f8a3928005c44ec5f196caca
zjopy <33468089+zjopy@users.noreply.github.com>
2020-01-10 04:34:09 +0100

    renamed oneQuoteADay folder to lowercase

---
+++ projects/onequoteaday/oneQuoteADay.js
@@ -0,0 +1,40 @@
+let allQuotes;
+
+// Todo: convert to promises
+
+function fetchAsset(url) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = "json";
+  request.send();
+  request.onload = function() {
+    if (request.status == 200) {
+      allQuotes = request.response;
+      getQuote();
+    } else {
+      throw Error(request.statusText);
+    }
+  };
+}
+
+const titleElement = document.querySelector("#title");
+const quoteElement = document.querySelector("#quote");
+
+// fetchAsset("https://api.myjson.com/bins/134p06"); // to be able to run locally
+fetchAsset("authenticagility.json");
+
+function getQuote() {
+
+  const numberOfQuotes = allQuotes.length;
+  const randomIndex = Math.floor(Math.random() * numberOfQuotes);
+  const quoteOfTheDay = allQuotes[randomIndex];
+
+  // use only quotes without the given note
+  if (quoteOfTheDay.note === "[duplicate]") {
+    getQuote();
+  } else {
+    titleElement.textContent = '- ' + quoteOfTheDay.category + ' -';
+    quoteElement.textContent = '\"' + quoteOfTheDay.question.replace("XYZ", "...") + '\"';
+  }
+
+}
